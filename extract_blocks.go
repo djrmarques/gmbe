610859da1ce21adedda08e04e817040692324c15
David Marques <davidmarques856@gmail.com>
2023-05-29 11:29:12 +0200

    Passed first test

--- extract_blocks.go
+++ extract_blocks.go
@@ -2,15 +2,46 @@ package main
 
 import (
 	"os"
+	"log"
+	"regexp"
+	"strings"
 )
+
+const BlockRegex = "```.*```"
 
 // Contains one code block
 type SourceBlock struct {
 	t, content string
 }
 
+// Extract a source block from a certain block in st
+func ParseBlock(b string) (block SourceBlock) {
+	// Trim the backquotes
+	b = strings.Trim(b, "`\n")
+
+	// Get the block type from the first line
+	lines := strings.Split(b, "\n")
+	t := lines[0]
+	content := strings.Join(lines[1:], "\n")
+	block = SourceBlock{t, content}
+	return
+	
+}
+
+// Given a file, extract all source blocks
+func ExtractBlocks(f []byte) (blocks []SourceBlock, err error) {
+	re := regexp.MustCompile(BlockRegex)
+	var s SourceBlock
+	for _, m := range re.FindAll(f, -1) {
+		log.Println(m)
+		s = SourceBlock{t: "asda", content: "sdad"}
+		blocks = append(blocks, s)
+	}
+	return
+}
+
 // Extracts all code blocks from a given file
-func ExtractBlocks(filePath string) (blocks []SourceBlock, err error) {
+func ExtractBlocksFromFile(filePath string) (blocks []SourceBlock, err error) {
 	data, err := os.ReadFile(filePath)
 	blocks = append(blocks, SourceBlock{t: "asda", content: string(data)})
 	return
--- extract_blocks_test.go
+++ extract_blocks_test.go
@@ -1,26 +1,39 @@
 package main
 
 import (
-	"path/filepath"
 	"testing"
 )
 
-func TestExtractBlocks(t *testing.T) {
-	var result [2]SourceBlock
-	result[0] = SourceBlock{t: "teser", content: "asd"}
-	result[1] = SourceBlock{t: "teser", content: "asd"}
+const TestString1 = "Text Here \n```yml\nSomeText\nMoreText\n```\nMore Text here"
+const TestString2 = "Text Here \n```\nSomeText\nMoreText\n```\nMore Text here"
+const TestBlock = "```yml\nwtv\n```"
 
-	path := filepath.Join(".", "test", "fixtures", "test.md")
-	blocks, err := ExtractBlocks(path)
-	if err != nil {
-		t.Error("Found error")
+func TestParseBlock(t *testing.T) {
+	block := ParseBlock(TestBlock)
+	result := SourceBlock{t: "yml", content: "wtv"}
+	if block != result {
+		t.Fatalf("Blocks do not match. Expected %v but got %+v", result, block)
 	}
-
-	for i := range result {
-		if result[i] != blocks[i] {
-			t.Error("Results do not match")			
-		}
-	}
+		
 }
 
-q
+// func TestExtractBlocksFromStr(t *testing.T) {
+// }
+
+// func TestExtractBlocksFromFile(t *testing.T) {
+// 	var result [2]SourceBlock
+// 	result[0] = SourceBlock{t: "teser", content: "asd"}
+// 	result[1] = SourceBlock{t: "teser", content: "asd"}
+
+// 	path := filepath.Join(".", "test", "fixtures", "test.md")
+// 	blocks, err := ExtractBlocksFromFile(path)
+// 	if err != nil {
+// 		t.Error("Found error")
+// 	}
+
+// 	for i := range result {
+// 		if result[i] != blocks[i] {
+// 			t.Error("Results do not match")			
+// 		}
+// 	}
+//}
