cfdc1d69974f16f87ba61d31e94f9bc634ed80e4
David Marques <davidmarques856@gmail.com>
2023-06-24 18:51:33 +0200

    Changed documentation

--- cmd/root.go
+++ cmd/root.go
@@ -14,14 +14,12 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "extractor",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "gmbe -f path/to/file.md",
+	Short: "Extract code blocks from Markdown files",
+	Long: `
+Go Markdown Block Extractor will extract all the codeblocks from markdown files and save them as their own separate files.
+This allows to run checks like linting, formating or any other custom checks to make sure that the code blocks in the markdown files, which most likely are some sort of documentation, are actually valid.
+`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
